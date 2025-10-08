37adc7a845cec720bf11cd4c30eff90e9bb4dae8
ousemaakacha <akachaousema@gmail.com>
2025-10-08 13:30:46 +0200

    last teacher

--- arrays 1/script.js
+++ arrays 1/script.js
@@ -10,10 +10,11 @@ teachers[4] = "Patrick";
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
