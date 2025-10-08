7528696faa1a00a0e8f7df552f8d8856b1ad75f9
ousemaakacha <akachaousema@gmail.com>
2025-10-08 23:30:58 +0200

    fabio ais present

--- arrays bouns/script.js
+++ arrays bouns/script.js
@@ -34,7 +34,14 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent = false;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === `Fabio`) {
+    isFabioPresent = true;
+  }
+}
+
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers;
