417a235efc9c7c01ab52456582d2beb4f83e03ad
ousemaakacha <akachaousema@gmail.com>
2025-10-08 21:44:19 +0200

    long names

--- arrays bouns/script.js
+++ arrays bouns/script.js
@@ -11,8 +11,8 @@ console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
-for (let i = 0; i > teachers.length; i++) {
-  if (teachers.length >= 5) {
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
     longNames.push(teachers[i]);
   }
 }
