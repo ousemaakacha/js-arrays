912cf50e1633334bd0f529404ffa7a428b6daa7a
ousemaakacha <akachaousema@gmail.com>
2025-10-08 14:06:08 +0200

    removed ed

--- arrays bouns/script.js
+++ arrays bouns/script.js
@@ -7,9 +7,14 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.length;
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const no_Ed = teachers.indexOf("Ed");
+if (no_Ed > -1) {
+  teachers.splice(no_Ed, 1);
+}
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
